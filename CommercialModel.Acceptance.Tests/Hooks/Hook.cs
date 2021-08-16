e07ce88140aea99f010077b29ed5c5c2fe2f018b
Wouter Simons <wsimons@schubergphilis.com>
2021-08-16 13:26:26 +0200

    Make acceptance tests run more simply

--- CommercialModel.Acceptance.Tests/Hooks/Hook.cs
+++ CommercialModel.Acceptance.Tests/Hooks/Hook.cs
@@ -1,11 +1,34 @@
 using System;
 using TechTalk.SpecFlow;
+using CommercialModelApi;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace CommercialModel.Acceptance.Tests.Hooks
 {
     [Binding]
     public class Hooks
     {
-        
+        private static CancellationTokenSource _cancelToken = new CancellationTokenSource();
+
+        [BeforeTestRun]
+        public static void StartApi()
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webbuilder =>
+                {
+                    webbuilder.UseStartup<Startup>();
+                })
+                .Build().RunAsync(_cancelToken.Token);
+        }
+
+        [AfterTestRun]
+        public static void StopApi()
+        {
+            _cancelToken.Cancel();
+        }
+
     }
 }
